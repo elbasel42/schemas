08125f3a628995ea4be38770c89090602b32dba8
alyadelaly <aliadelhacker123@yahoo.com>
2025-04-10 12:01:28 +0200

    update vacation typesschema

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-export { ResponseSchema } from "./responseSchema";
-export { ResultSchema } from "./resultSchema";
+export { ResponseSchema } from './responseSchema';
+export { ResultSchema } from './resultSchema';
 
-export { HolidayElementSchema, type HolidayElement } from "./holiday/schema";
+export { HolidayElementSchema, type HolidayElement } from './holiday/schema';
 
 export {
   ChangeBankAccountElementSchema,
@@ -10,25 +10,26 @@ export {
 export {
   MedicalInsuranceElementSchema,
   type MedicalInsuranceElement
-} from "./medical-insurance/schema";
+} from './medical-insurance/schema';
 export {
   PermissionElementSchema,
   type PermissionElement
-} from "./permission/schema";
+} from './permission/schema';
 export {
   RemoteWorkElementSchema,
   type RemoteWorkElement
-} from "./remote-work/schema";
+} from './remote-work/schema';
 export {
   RequestCreateWorkflowElementSchema,
   type RequestCreateWorkflowElement
-} from "./request-create-workflow/schema";
+} from './request-create-workflow/schema';
 export {
   RequestDetailsWorkflowElementSchema,
   type RequestDetailsWorkflowElement
-} from "./request-details-workflow/schema";
+} from './request-details-workflow/schema';
 export {
   SalaryIdentificationElementSchema,
   type SalaryIdentificationElement
-} from "./salary-identification/schema";
+} from './salary-identification/schema';
+export { VacationTypeSchema, type VacationType } from './vacation-types/schema';
 
