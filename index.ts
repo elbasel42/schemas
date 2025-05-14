3081a297dec5d8dd980d69933d09b4009950c71e
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-05-14 11:44:04 +0300

    generate ad-news schema and add to index.ts

--- index.ts
+++ index.ts
@@ -1,45 +1,47 @@
-export { ResponseSchema } from './responseSchema';
-export { ResultSchema } from './resultSchema';
+export { ResponseSchema } from "./responseSchema";
+export { ResultSchema } from "./resultSchema";
 
-export { HolidayElementSchema, type HolidayElement } from './holiday/schema';
+export { HolidayElementSchema, type HolidayElement } from "./holiday/schema";
 
 export {
   ChangeBankAccountElementSchema,
-  type ChangeBankAccountElement
-} from './change-bank-account/schema';
-export { CustodyElementSchema, type CustodyElement } from './custody/schema';
+  type ChangeBankAccountElement,
+} from "./change-bank-account/schema";
+export { CustodyElementSchema, type CustodyElement } from "./custody/schema";
 export {
   DestinationElementSchema,
-  type DestinationElement
-} from './destination/schema';
+  type DestinationElement,
+} from "./destination/schema";
 export {
   HrLetterTypeSchema,
-  type HrLetterType
-} from './hr-letter-types/schema';
+  type HrLetterType,
+} from "./hr-letter-types/schema";
 export {
   MedicalInsuranceElementSchema,
-  type MedicalInsuranceElement
-} from './medical-insurance/schema';
+  type MedicalInsuranceElement,
+} from "./medical-insurance/schema";
 // export {
 //   PermissionElementSchema,
 //   type PermissionElement
 // } from './permission/schema';
+export { AdNewSchema } from "./ad-news/schema";
+export { FamilyNewSchema } from "./family-news/schema";
+export { NewsElementSchema } from "./news/schema";
 export {
   RemoteWorkElementSchema,
-  type RemoteWorkElement
-} from './remote-work/schema';
+  type RemoteWorkElement,
+} from "./remote-work/schema";
 export {
   RequestCreateWorkflowElementSchema,
-  type RequestCreateWorkflowElement
-} from './request-create-workflow/schema';
+  type RequestCreateWorkflowElement,
+} from "./request-create-workflow/schema";
 export {
   RequestDetailsWorkflowElementSchema,
-  type RequestDetailsWorkflowElement
-} from './request-details-workflow/schema';
+  type RequestDetailsWorkflowElement,
+} from "./request-details-workflow/schema";
 export {
   SalaryIdentificationElementSchema,
-  type SalaryIdentificationElement
-} from './salary-identification/schema';
-export { TrainingElementSchema, type TrainingElement } from './training/schema';
-export { VacationTypeSchema, type VacationType } from './vacation-types/schema';
-
+  type SalaryIdentificationElement,
+} from "./salary-identification/schema";
+export { TrainingElementSchema, type TrainingElement } from "./training/schema";
+export { VacationTypeSchema, type VacationType } from "./vacation-types/schema";
